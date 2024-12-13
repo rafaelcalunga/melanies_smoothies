3cd91a5c144e1fab03cfaa1ecc01ccb24b6a5770
Rafael Calunga <rafaelcalunga@gmail.com>
2024-12-13 13:55:08 -0500

    Add requests package

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 cnx = st.connection('snowflake')
 session = cnx.session()
@@ -37,3 +38,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon='✅')
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
